e4c34351a205479d8914b65032249cb170fb6ea3
Nikita <nikita.pymitron@gmail.com>
2025-07-21 14:00:43 +0700

    phone number in letters parser, date parser from hex to readable format

--- src/pdu_decoder.lua
+++ src/pdu_decoder.lua
@@ -41,20 +41,31 @@ function pdu_decoder.parse(msg)
     print("[pdu_decoder.lua] sender_address_type: ", sender_address_type)
     p = p + 2
 
-    local sender_number = "+"
-    for i = 0, math.ceil(tonumber(sender_address_length, 16) / 2) - 1 do
-        local digits = msg:sub(p, p + 1)
-        p = p + 2
+    local sender_number = ""
+    if sender_address_type == "91" then
+        sender_number = "+"
+        for i = 0, math.ceil(tonumber(sender_address_length, 16) / 2) - 1 do
+            local digits = msg:sub(p, p + 1)
+            p = p + 2
 
-        local first_digit = digits:sub(2, 2)
-        local second_digit = digits:sub(1, 1)
+            local first_digit = digits:sub(2, 2)
+            local second_digit = digits:sub(1, 1)
 
-        if i == math.ceil(tonumber(sender_address_length, 16) / 2) - 1 then
-            second_digit = ""
+            if i == math.ceil(tonumber(sender_address_length, 16) / 2) - 1 then
+                second_digit = ""
+            end
+
+            sender_number = sender_number .. first_digit .. second_digit
         end
-
-        sender_number = sender_number .. first_digit .. second_digit
+    elseif sender_address_type == "D0" then
+        for i = 0, math.ceil(tonumber(sender_address_length, 16) / 2) - 1 do
+            local digits = msg:sub(p, p + 1)
+            sender_number = sender_number .. digits:sub(1, 2)
+            p = p + 2
+        end
+        sender_number = text_decoder.gsm7bit_to_text(sender_number)
     end
+
     print("[pdu_decoder.lua] sender_number: ", sender_number)
 
     local protocol_identifier = msg:sub(p, p + 1)
@@ -65,17 +76,40 @@ function pdu_decoder.parse(msg)
     print("[pdu_decoder.lua] data_coding_scheme: ", data_coding_scheme)
     p = p + 2
 
-    local date = ""
+    local date = {
+        text = "",
+        year = "",
+        month = "",
+        day = "",
+        hours = "",
+        minutes = "",
+        seconds = "",
+        time_zone = "",
+    }
+
     for i = 0, 6 do
         local digits = msg:sub(p, p + 1)
+        local date_number = digits:sub(2, 2) .. digits:sub(1, 1)
         p = p + 2
 
-        local first_digit = digits:sub(2, 2)
-        local second_digit = digits:sub(1, 1)
-
-        date = date .. first_digit .. second_digit .. " "
+        if i == 0 then
+            date.year = date_number
+        elseif i == 1 then
+            date.month = date_number
+        elseif i == 2 then
+            date.day = date_number
+        elseif i == 3 then
+            date.hours = date_number
+        elseif i == 4 then
+            date.minutes = date_number
+        elseif i == 5 then
+            date.seconds = date_number
+        elseif i == 6 then
+            date.time_zone = date_number
+        end
     end
-    print("[pdu_decoder.lua] date: ", date)
+    print("[pdu_decoder.lua] date: ", date.text)
+    date.text = date.day .. '.' .. date.month .. '.' .. date.year .. ' ' .. date.hours .. ':' .. date.minutes .. ':' .. date.seconds .. ' ' .. 'GMT+' .. tostring(tonumber(date.time_zone, 10) * 15 / 60)
 
     local user_date_length = msg:sub(p, p + 1)
     print("[pdu_decoder.lua] user_date_length: ", user_date_length)
@@ -97,6 +131,7 @@ function pdu_decoder.parse(msg)
         message = message_hex,
         decoded_message = message_text,
         data_coding_scheme = data_coding_scheme,
+        date = date.text,
     }
 end
 
