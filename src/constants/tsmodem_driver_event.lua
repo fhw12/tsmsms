26678c0fb8a02045f7bd53591f3ddff7e40edd8a
Nikita <nikita.pymitron@gmail.com>
2025-07-25 13:37:45 +0700

    tsmodem driver event constants

---
+++ src/constants/tsmodem_driver_event.lua
@@ -0,0 +1,8 @@
+local tsmodem_driver_event = {
+    SMS_SENT_OK = "SMS-SENT-OK",
+    SMS_SENT_ERROR = "SMS-SENT-ERROR",
+    AT_ANSWER = "AT-ANSWER",
+    SMS_RECEIVED = "SMS-RECEIVED",
+}
+
+return tsmodem_driver_event
